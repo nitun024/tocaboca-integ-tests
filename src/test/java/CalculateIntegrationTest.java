093041dd33d23720e94747474b0b989e344e07ed
Nitun Pachauri <pachnitu@amazon.com>
2021-12-29 20:51:41 +0530

    initial commit

---
+++ src/test/java/CalculateIntegrationTest.java
@@ -0,0 +1,12 @@
+//import org.testng.*;
+
+import org.testng.annotations.Test;
+
+public class CalculateIntegrationTest {
+
+    @Test
+     public void testCalculateExpression(){
+        System.out.println("Test has successfully passed.");
+     }
+
+}
